ce625099d04adc0dd494ec2223d460adc4d7d55a
Zachary Schlaich <zachschlaich1998@gmail.com>
2025-01-26 22:18:18 -0500

    Add new classes for Players and Teams

---
+++ Models/Player.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json.Linq;
+using static FantasyFootballBot.Constants;
+
+namespace FantasyFootballBot.Models
+{
+    public class Player
+    {
+        // Player personal info
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName { get; set; }
+
+        // Fantasy info
+        public string PlayerId { get; set; }
+        public string TeamCode { get; set; }
+        public List<PlayerPositions> Positions { get; set; } = [];
+
+        public Player(JObject playerJson)
+        {
+            FirstName = (string)playerJson.SelectToken("first_name")!;
+            LastName = (string)playerJson.SelectToken("last_name")!;
+            FullName = (string)playerJson.SelectToken("full_name")!;
+            PlayerId = (string)playerJson.SelectToken("player_id")!;
+            TeamCode = (string)playerJson.SelectToken("team")!;
+
+            var fantasyPositions = playerJson.SelectToken("fantasy_positions")!.ToList();
+            var playerPositions = new List<PlayerPositions>();
+            foreach (var position in fantasyPositions)
+            {
+                if (Enum.TryParse(position.ToString(), out PlayerPositions playerPosition)) playerPositions.Add(playerPosition);
+            }
+            Positions = playerPositions;
+        }
+
+        public override string ToString()
+        {
+            return $"'{FullName} ({String.Join(", ", Positions)}), {TeamCode}, {PlayerId}'";
+        }
+    }
+}
---
+++ Models/Team.cs
@@ -0,0 +1,95 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace FantasyFootballBot.Models
+{
+    public class Team
+    {
+        // Team info
+        public string LeagueId { get; set; }
+        public string OwnerId { get; set; }
+        public string Name { get; set; }
+
+        // Team record info
+        public int Wins { get; set; }
+        public int Losses { get; set; }
+        public int Ties { get; set; }
+
+        // Team roster management info
+        public int TotalMoves { get; set; }
+        public int WaiverBudget { get; set; }
+        public int WaiverBudgetUsed { get; set; }
+        public int WaiverBudgetRemaining { get; set; }
+
+        // Roster info
+        private List<string>? StartersIds;
+        private List<string>? BenchIds;
+        private List<string>? TaxiIds;
+        public List<Player>? Starters { get; set; }
+        public List<Player>? Bench { get; set; }
+        public List<Player>? Taxi { get; set; }
+
+        // Team scoring info
+        public float FptsFor { get; set; }
+        public int FptsForWhole { get; set; }
+        public int FptsForDecimal { get; set; }
+        public float FptsAgainst { get; set; }
+        public int FptsAgainstWhole { get; set; }
+        public int FptsAgainstDecimal { get; set; }
+        public float Ppts { get; set; }
+        public int PptsWhole { get; set; }
+        public int PptsDecimal { get; set; }
+
+        public Team(JObject teamJson)
+        {
+            LeagueId = (string)teamJson.SelectToken("league_id")!;
+            OwnerId = (string)teamJson.SelectToken("owner_id")!;
+            Name = (string)teamJson.SelectToken("team_name")!;
+
+            Wins = (int)teamJson.SelectToken("settings.wins")!;
+            Losses = (int)teamJson.SelectToken("settings.losses")!;
+            Ties = (int)teamJson.SelectToken("settings.ties")!;
+
+            TotalMoves = (int)teamJson.SelectToken("settings.total_moves")!;
+            WaiverBudget = Constants.waiverBudget;
+            WaiverBudgetUsed = (int)teamJson.SelectToken("settings.waiver_budget_used")!;
+            WaiverBudgetRemaining = WaiverBudget - WaiverBudgetUsed;
+
+            UpdateRoster(teamJson);
+
+            FptsForWhole = (int)teamJson.SelectToken("settings.fpts")!;
+            FptsForDecimal = (int)teamJson.SelectToken("settings.fpts_decimal")!;
+            FptsFor = (float)FptsForWhole + (float)FptsForDecimal / 100;
+
+            FptsAgainstWhole = (int)teamJson.SelectToken("settings.fpts_against")!;
+            FptsAgainstDecimal = (int)teamJson.SelectToken("settings.fpts_against_decimal")!;
+            FptsAgainst = (float)FptsAgainstWhole + (float)FptsAgainstDecimal / 100;
+
+            PptsWhole = (int)teamJson.SelectToken("settings.ppts")!;
+            PptsDecimal = (int)teamJson.SelectToken("settings.ppts_decimal")!;
+            Ppts = (float)PptsWhole + (float)PptsDecimal / 100;
+        }
+
+        /// <summary>
+        /// Update's a team's roster to include new players and where they are located on the current roster (starter/bench/taxi).
+        /// </summary>
+        public void UpdateRoster(JObject teamJson)
+        {
+            StartersIds = teamJson.SelectToken("starters")!.ToObject<List<string>>()!;
+            TaxiIds = teamJson.SelectToken("taxi")!.ToObject<List<string>>()!;
+            var players = teamJson.SelectToken("players")!.ToObject<List<string>>()!;
+            BenchIds = [];
+            foreach (var playerId in players)
+            {
+                if (!StartersIds.Contains(playerId))
+                {
+                    BenchIds.Add(playerId);
+                }
+            };
+        }
+
+        public override string ToString()
+        {
+            return $"Team name: '{Name}', Owner id: '{OwnerId}'";
+        }
+    }
+}
