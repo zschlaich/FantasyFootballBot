98cb33d97c752f7d2ed2b56dc182ab6e676a641b
Zachary Schlaich <zachschlaich1998@gmail.com>
2025-01-26 22:15:22 -0500

    Update Constants.cs with new values

--- Constants.cs
+++ Constants.cs
@@ -19,7 +19,52 @@
         public const ulong pooKingUserId = 695431484604940351;
 
         // Fantasy Scoring
+        // TODO look into grabbing this information directly from Sleeper in the future.
+        // Passing
+        public const float passYard = 0.04F;
+        public const float passTd = 4.0F;
+        public const float pass2ptConversion = 2.0F;
+        public const float passInt= -2.0F;
+        // Rushing
+        public const float rushYard = 0.1F;
+        public const float rushTd = 6.0F;
+        public const float rush2ptConversion = 2.0F;
+        public const float rushFumbleLost = -2.0F;
+        // Receiving
+        public const float recReception = 0.5F;
+        public const float recYard = 0.1F;
+        public const float rec2ptConversion = 2.0F;
+        public const float recTd = 6.0F;
+        // Defense
+        // Kicking
 
         // Fantasy League Info
+        public const string leagueId = "1089364456892686336";
+        public const int waiverBudget = 100;
+
+        // Fantasy Football values
+        public enum PlayerPositions
+        {
+            QB,
+            RB,
+            WR,
+            TE,
+            K,
+            DEF,
+            DB,
+            DL,
+            LB,
+        }
+        public enum RosterPositions
+        {
+            QB,
+            RB,
+            WR,
+            TE,
+            FLEX,
+            SUPER_FLEX,
+            BN,
+            IR,
+        }
     }
 }
